2b47406b9fb0b7f229d45f8a8706a6f0b189515f
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-06-10 15:20:27 -0700

    Move to an app namespace.

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import * as k8s from "@pulumi/kubernetes";
 
+const namespace = "apps-ns";
+const ns = new k8s.core.v1.Namespace(namespace, {
+  metadata: {
+    name: namespace
+  }
+});
 const appLabels = { app: "nginx" };
 const deployment = new k8s.apps.v1.Deployment("nginx", {
+  metadata: {
+    namespace
+  },
   spec: {
     selector: { matchLabels: appLabels },
     replicas: 1,
