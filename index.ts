9d52c9f5b11a4035b99275c3383fb2e7fba27902
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-06-10 16:46:29 -0700

    Use a namespace for the deployment.

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import * as k8s from "@pulumi/kubernetes";
 
+const namespace = "apps";
 const appLabels = { app: "nginx" };
+
+const appNs = new k8s.core.v1.Namespace("apps-ns", {
+  metadata: {
+    namespace
+  }
+});
 const deployment = new k8s.apps.v1.Deployment("nginx", {
+  metadata: {
+    namespace
+  },
   spec: {
     selector: { matchLabels: appLabels },
     replicas: 1,
