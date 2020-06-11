4318af996cdb2090f3bd69a714527945bd4b26f1
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-06-11 09:38:58 -0700

    Add a ConfigMap resource.

--- index.ts
+++ index.ts
@@ -1,23 +1,29 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const namespace = "apps";
 const appLabels = { app: "nginx" };
 
-const appNs = new k8s.core.v1.Namespace("apps-ns", {
-  metadata: {
-    name: namespace,
-  }
-});
-const deployment = new k8s.apps.v1.Deployment("nginx", {
-  metadata: {
-    namespace
+// Create a ConfigMap for the nginx container.
+const configMap = new k8s.core.v1.ConfigMap("config-map", {
+  data: {
+    someInterestingKey: "interestingValue"
   },
+});
+
+const deployment = new k8s.apps.v1.Deployment("nginx", {
   spec: {
     selector: { matchLabels: appLabels },
     replicas: 1,
     template: {
       metadata: { labels: appLabels },
-      spec: { containers: [{ name: "nginx", image: "nginx" }] }
+      spec: {
+        containers: [{
+          name: "nginx",
+          image: "nginx",
+          envFrom: [{
+            configMapRef: { name: configMap.metadata.name }
+          }]
+        }]
+      }
     }
   }
 });
