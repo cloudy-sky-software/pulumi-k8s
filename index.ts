fa06348e4dfa07bc13b9d49fdc94a20bf98a3db2
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-06-11 11:17:28 -0700

    Revert "Add namespaces."
    This reverts commit 6c077dddcc8c584ecff54a87b9b837f22ac93d25.

--- index.ts
+++ index.ts
@@ -1,29 +1,15 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const namespace = "apps";
 const appLabels = { app: "nginx" };
-
-// Create a namespace for all "apps" resources in the cluster.
-const appNs = new k8s.core.v1.Namespace("apps-ns", {
-  metadata: {
-    name: namespace,
-  }
-});
 
 // Create a ConfigMap for the nginx container.
 const configMap = new k8s.core.v1.ConfigMap("config-map", {
-  metadata: {
-    namespace
-  },
   data: {
     someInterestingKey: "interestingValue"
   },
 });
 
 const deployment = new k8s.apps.v1.Deployment("nginx", {
-  metadata: {
-    namespace
-  },
   spec: {
     selector: { matchLabels: appLabels },
     replicas: 1,
