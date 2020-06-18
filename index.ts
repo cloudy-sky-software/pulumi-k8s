ec8b16fda2d4fbda08acbb29490aae5eded44340
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-06-18 09:08:20 -0700

    Revert to core example.

--- index.ts
+++ index.ts
@@ -1,29 +1,7 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const namespace = "apps";
 const appLabels = { app: "nginx" };
-
-// Create a namespace for all "apps" resources in the cluster.
-const appNs = new k8s.core.v1.Namespace("apps-ns", {
-  metadata: {
-    name: namespace,
-  }
-});
-
-// Create a ConfigMap for the nginx container.
-const configMap = new k8s.core.v1.ConfigMap("config-map", {
-  metadata: {
-    namespace
-  },
-  data: {
-    someInterestingKey: "interestingValue"
-  },
-});
-
 const deployment = new k8s.apps.v1.Deployment("nginx", {
-  metadata: {
-    namespace
-  },
   spec: {
     selector: { matchLabels: appLabels },
     replicas: 1,
@@ -33,9 +11,6 @@ const deployment = new k8s.apps.v1.Deployment("nginx", {
         containers: [{
           name: "nginx",
           image: "nginx",
-          envFrom: [{
-            configMapRef: { name: configMap.metadata.name }
-          }]
         }]
       }
     }
